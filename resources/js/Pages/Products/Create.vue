9c9ddffc8dbd0059006b3fd362ea4b94e8c8e6d0
SolObwot <solobwot@gmail.com>
2025-07-11 15:11:00 +0300

    Add Create page for Loan Leads with initial layout and structure

---
+++ resources/js/Pages/Products/Create.vue
@@ -0,0 +1,28 @@
+<template>
+    <AppLayout title="Loan Leads">
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                Loan Leads
+            </h2>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 bg-white border-b border-gray-200">
+                        Coming Soon
+                    </div>
+                </div>
+            </div>
+        </div>
+    </AppLayout>
+</template>
+
+<script>
+    import AppLayout from '@/Layouts/AppLayout.vue';
+    export default {
+        components: {
+            AppLayout,
+        }
+    }
+</script>
